6820fcecacab4171927b82711535b7d50e6a8bc5
Van Dong <vandong78py@gmail.com>
2025-02-26 11:36:22 +0700

    feat: done create customFetch

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import "./index.css";
+import customFetch from "./utils/customFetch";
 // import App from "./App.jsx";
 
-fetch("/api/v1/get-allcode?type=test")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const data = await customFetch.get("/get-allcode?type=ROLE");
+console.log(data);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
