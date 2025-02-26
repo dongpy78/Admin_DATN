ebdf435b28f6a2f2ea7d50ba6c61deb942a0fac9
Van Dong <vandong78py@gmail.com>
2025-02-26 09:25:40 +0700

    feat: setup proxy to call api

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import router from "./routes";
 import "./index.css";
 // import App from "./App.jsx";
 
+fetch("/api/v1/get-allcode?type=test")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
